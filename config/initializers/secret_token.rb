39445ee55e31ade85f4f16faecb69d8319aef114
Tim Hui <timboh56@gmail.com>
2013-02-26 19:17:05 -0500

    redo init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtrep::Application.config.secret_token = 'b35bad4ad729529c51df4cfd7a4362d540b13086b57dbdaae5bad288e296d6fc8a1fffd19db1368fb5d4a3eca4232a53e0556d51ca400957347b766bead5c000'
