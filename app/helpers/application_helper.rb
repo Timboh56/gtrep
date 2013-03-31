73ccb52ac9220ab4bbec212ffaaf2ba783c8aa10
Tim Hui <timboh56@gmail.com>
2013-03-30 19:35:47 -0700

    added hover effects for rows in tables, added string mod methods in application helper for table fields with regex stuff, added index_table to responses index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,30 @@
 module ApplicationHelper
   def index_table(object, klass) 
     render "layouts/index_table", {
-      :object => object, 
+      :object => object,
       :klass => klass,
       :fields => klass.column_names[1,klass.column_names.length]
     }
   end
   
+  # normalizeField will take a field as a string and return it without "_id". e.g. user_id becomes user
+  def normalizeField(string)
+    string.gsub /_id|_/, " "
+  end
+  
+  # uncapitalize makes input string completely lower case
   def uncapitalize(string)
     string[0, string.length].downcase
   end
+  
+  # filterObject removes input fields (an array of strings to be matched) from object
+  def filterObject(object, fields)
+    regex = fields.join("|")
+    object.delete_if { |key, value| key.to_s =~ /^#{regex}/m }
+  end
+  
+  def username(id)
+    User.find(id).username rescue "N/A"
+  end
+  
 end
