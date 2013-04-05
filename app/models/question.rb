9ea48b6bb8e4a5ec163e0ef8bc36ded8da9fce54
Tim Hui <timboh56@gmail.com>
2013-04-04 18:21:33 -0700

    added foreign key constraints on questions to question_group questions and stuff

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   attr_accessible :category_id, :text, :type, :question_type, :answers, :open
   belongs_to :categories
   belongs_to :question_groups
-  has_many :answers   
+  has_many :answers, :dependent => :destroy
+  has_many :question_group_questions, :dependent => :destroy
   using_access_control
 end
